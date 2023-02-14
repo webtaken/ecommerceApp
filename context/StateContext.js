9883787578de742647c120ee645aecf7ccf81d43
saul <luckly083@gmail.com>
2023-02-14 12:24:45 -0500

    Adding initial logic for cart component

--- context/StateContext.js
+++ context/StateContext.js
@@ -6,22 +6,62 @@ const Context = createContext();
 
 export const StateContext = ({ children }) => {
   const [showCart, setShowCart] = useState(false);
-  const [cartItems, setCartItems] = useState();
-  const [totalPrice, setTotalPrice] = useState();
-  const [totalQuantities, setTotalQuantities] = useState();
+  const [cartItems, setCartItems] = useState([]);
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [totalQuantities, setTotalQuantities] = useState(0);
   const [qty, setQty] = useState(1);
+
+  const onAdd = (product, quantity) => {
+    const checkProductInCart = cartItems.find(
+      (item) => item._id === product._id
+    );
+    if (checkProductInCart) {
+      setTotalPrice(
+        (prevTotalPrice) => prevTotalPrice + product.price * quantity
+      );
+      const updatedCartItems = cartItems.map((cartProduct) => {
+        if (cartProduct._id === product._id)
+          return {
+            ...cartProduct,
+            quantity: cartProduct.quantity + quantity,
+          };
+      });
+      setCartItems(updatedCartItems);
+    } else {
+      product.quantity = quantity;
+      setCartItems([...cartItems, { ...product }]);
+    }
+    setTotalQuantities(
+      (prevTotalQuantities) => prevTotalQuantities + quantity
+    );
+    toast.success(`${qty} ${product.name} added to the cart.`);
+  };
+
+  const incQty = () => {
+    setQty((prevQty) => prevQty + 1);
+  };
+
+  const decQty = () => {
+    setQty((prevQty) => Math.max(1, prevQty - 1));
+  };
 
   return (
     <Context.Provider
       value={{
         showCart,
+        setShowCart,
         cartItems,
         totalPrice,
         totalQuantities,
         qty,
+        incQty,
+        decQty,
+        onAdd,
       }}
     >
       {children}
     </Context.Provider>
   );
 };
+
+export const useStateContext = () => useContext(Context);
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -7,10 +7,13 @@ import {
 } from "react-icons/ai";
 import { Product } from "@/components";
 import { client, urlFor } from "@/lib/client";
+import { useStateContext } from "../../context/StateContext";
 
 const ProductDetails = ({ product, products }) => {
   const [imgIndex, setImgIndex] = useState(0);
   const { image, name, details, price } = product;
+  const { decQty, incQty, qty, onAdd } = useStateContext();
+
   return (
     <div>
       <div className="product-detail-container">
@@ -54,19 +57,19 @@ const ProductDetails = ({ product, products }) => {
           <div className="quantity">
             <h3>Quantity:</h3>
             <p className="quantity-desc">
-              <span className="minus" onClick="">
+              <span className="minus" onClick={decQty}>
                 <AiOutlineMinus />
               </span>
               <span className="num" onClick="">
-                0
+                {qty}
               </span>
-              <span className="plus" onClick="">
+              <span className="plus" onClick={incQty}>
                 <AiOutlinePlus />
               </span>
             </p>
           </div>
           <div className="buttons">
-            <button type="button" className="add-to-cart" onClick="">
+            <button type="button" className="add-to-cart" onClick={() => onAdd(product, qty)}>
               Add to Cart
             </button>
             <button type="button" className="buy-now" onClick="">
